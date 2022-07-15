a703cb628fa120aed12c3d15bc70be08944f20c3
lZiobro <303206@stud.umk.pl>
2022-07-15 03:00:01 +0200

    allow github.io origin

--- ExampleProjectAPI/Program.cs
+++ ExampleProjectAPI/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "CorsPolicy",
         builder =>
         {
-            builder.WithOrigins("http://localhost:3000")
+            builder.WithOrigins("http://localhost:3000", "https://lziobro.github.io")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod()
                                 .AllowCredentials();
